16144176140b27cffdc92a76f52455b153dcd695
Roman Starner <46463722+rstarner@users.noreply.github.com>
2019-03-10 23:49:20 -0400

    Challenge 2

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        Vector3 v = startPos;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+
+
+    public float delta = 1.5f;  // Amount to move left and right from the start point
+    public float speed = 2.0f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameControl.health -= 1;
+
+        
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,46 +1,116 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
-    public float speed;
-    public float jumpForce;
+    public float moveForce = 100f;
+    public float maxSpeed = 5f;
+    public float jumpForce = 1000f;
+    public Text countText;
+    public Text winText;
+    public bool facingRight = true;
+
+    private int count;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>(); 
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+        count = 0;
+
+        winText.text = "";
+
+        SetCountText();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        float h = Input.GetAxis("Horizontal");
 
-        Vector2 movement = new Vector2(moveHorizontal, 0);
 
-        rb2d.AddForce(movement * speed);
+        if (h * rb2d.velocity.x < maxSpeed)
+            rb2d.AddForce(Vector2.right * h * moveForce);
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxSpeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxSpeed, rb2d.velocity.y);
+
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
 
         if (Input.GetKey("escape"))
             Application.Quit();
 
     }
 
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("Pickup"))
+
+            other.gameObject.SetActive(false);
+
+        count = count + 1;
+
+        SetCountText();
+
+        if (other.gameObject.CompareTag("Enemy"))
+            Destroy(other.gameObject);
+
+
+
+
+
+
+
+
+    }
+
+
+
+    void SetCountText()
+    {
+
+        countText.text = "Score: " + count.ToString();
+
+        if (count >= 4)
+
+            winText.text = "You win!";
+
+
+    }
+
     void OnCollisionStay2D(Collision2D collision)
     {
-        if(collision.collider.tag == "Ground")
+        if (collision.collider.tag == "Ground")
         {
-            if(Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKey(KeyCode.UpArrow))
             {
                 rb2d.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             }
         }
     }
+
 }
